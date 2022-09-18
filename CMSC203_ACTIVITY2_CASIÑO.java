862dc5c5dbf56ff763444b185f76c11ce490e47b
kurtsebastian <112452838+kurtsebastian@users.noreply.github.com>
2022-09-18 23:26:40 +0800

    Create CMSC203_ACTIVITY2_CASIÑO.java

---
+++ CMSC203_ACTIVITY2_CASIÑO.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class arithmetic {
+    
+    public static void main(String[] args) {
+
+        int num1;
+        int num2 ;
+       
+Scanner scan = new Scanner(System.in);
+
+System.out.print("NUM1 :");
+num1= scan.nextInt();
+
+System.out.print("NUM2 :");
+num2 = scan.nextInt();
+
+
+System.out.println(num1 + num2);
+
+
+    }
+}
